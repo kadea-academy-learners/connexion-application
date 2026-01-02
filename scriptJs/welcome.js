6102e785758d12f095d5519afb34136402286ba0
vanguleprince <vangu.leprince@gmail.com>
2026-01-02 13:16:17 +0000

    feat / j'ai dynamiser mon application en creant automatiser avec mon js et ensuitte rajouter manuelement les articles dans ma base de donnée avant d'integrer la fonctionnalité de panier

---
+++ scriptJs/welcome.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+  chargerProduits()
+})
+
+function chargerProduits() {
+  fetch("http://localhost:3000/produits")
+    .then(response => response.json())
+    .then(produits => {
+      afficherProduits(produits)
+    })
+    .catch(error => {
+      console.log("Cette requête n'est pas disponible :", error)
+    })
+}
+
+function afficherProduits(produits) {
+  produits.forEach(produit => {
+
+    const section = document.getElementById(produit.categories)
+    if (!section) return
+
+    const article = document.createElement("article")
+    article.className =
+      "w-52 h-full flex-shrink-0 border rounded-lg p-2 shadow hover:scale-105 transition"
+
+    article.innerHTML = `
+      <img src="${produit.image}" 
+           alt="${produit.nom}"
+           class="w-full h-40 object-cover rounded">
+
+      <h3 class="mt-2 text-center text-sm font-bold">${produit.nom}</h3>
+
+      <p class="text-orange-600 text-center font-semibold">
+        ${produit.prix} $
+      </p>
+     
+      <div class=" justify-content ">
+
+      <button 
+        class="mt-2 w-40 bg-orange-500 text-white py-1 rounded hover:bg-orange-600"
+        data-id="${produit.id}">
+        Ajouter au panier
+      </button>
+      </div>
+    `
+
+    section.appendChild(article)
+  })
+}
