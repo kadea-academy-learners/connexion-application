041e01cff7a1dc3e060f5b4f4ab76f02893acbd0
vanguleprince <vangu.leprince@gmail.com>
2026-01-04 08:13:35 +0000

    fix / j'ai fini avec mon catalogue des produits maintenant je commence la partie panier de mon application

--- scriptJs/welcome.js
+++ scriptJs/welcome.js
@@ -1,49 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-  chargerProduits()
-})
+  chargerProduits();
+});
 
 function chargerProduits() {
   fetch("http://localhost:3000/produits")
     .then(response => response.json())
     .then(produits => {
-      afficherProduits(produits)
+      afficherProduits(produits);
     })
     .catch(error => {
-      console.log("Cette requête n'est pas disponible :", error)
-    })
+      console.log("Cette requête n'est pas disponible :", error);
+    });
 }
 
 function afficherProduits(produits) {
   produits.forEach(produit => {
 
-    const section = document.getElementById(produit.categories)
-    if (!section) return
+    const section = document.getElementById(produit.categories);
+    if (!section) return;
 
-    const article = document.createElement("article")
+    const article = document.createElement("article");
     article.className =
-      "w-52 h-full flex-shrink-0 border rounded-lg p-2 shadow hover:scale-105 transition"
+      "w-52 h-full flex-shrink-0 border rounded-lg p-2 shadow hover:scale-105 transition";
 
     article.innerHTML = `
       <img src="${produit.image}" 
-           alt="${produit.nom}"
+           alt="${produit.name}"
            class="w-full h-40 object-cover rounded">
 
-      <h3 class="mt-2 text-center text-sm font-bold">${produit.nom}</h3>
+      <h3 class="mt-2 text-center text-sm font-bold">${produit.name}</h3>
 
       <p class="text-orange-600 text-center font-semibold">
-        ${produit.prix} $
+        ${produit.price} $
       </p>
      
-      <div class=" justify-content ">
+      <div class="flex justify-center">
 
       <button 
-        class="mt-2 w-40 bg-orange-500 text-white py-1 rounded hover:bg-orange-600"
+        class="mt-2 text-center w-40 bg-orange-500 text-white py-1 rounded hover:bg-orange-600"
         data-id="${produit.id}">
         Ajouter au panier
       </button>
       </div>
-    `
+    `;
 
-    section.appendChild(article)
+    section.appendChild(article);
   })
 }
