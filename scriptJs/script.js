262f163e4ee383f24af243e304dcb8a9cfbeafce
vanguleprince <vangu.leprince@gmail.com>
2026-01-07 13:43:03 +0000

    fix / mise à jour

--- scriptJs/script.js
+++ scriptJs/script.js
@@ -1,43 +1,39 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const formulaire = document.getElementById("register");
-  const myNames = document.getElementById("names");
-  const myMail = document.getElementById("email");
-  const myAge = document.getElementById("ages");
-  const myPassword = document.getElementById("passwords");
+const formulaire = document.getElementById("register");
+const myNames = document.getElementById("names");
+const myMail = document.getElementById("email");
+const myAge = document.getElementById("ages");
+const myPassword = document.getElementById("passwords");
+document.getElementById("toast-container")
 
-  formulaire.addEventListener("submit", async (e) => {
-    e.preventDefault();
+formulaire.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    const userData = {
-      names: myNames.value.trim(),
-      emails: myMail.value.trim(),
-      age: myAge.value,
-      password: myPassword.value,
-    };
+  const userData = {
+    names: myNames.value.trim(),
+    emails: myMail.value.trim(),
+    age: myAge.value,
+    password: myPassword.value,
+  };
 
-    try {
-      const response = await fetch("http://localhost:3000/users", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(userData),
-      });
+  try {
+    const response = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userData),
+    });
 
     if (!response.ok) {
-  throw new Error("Échec de l’enregistrement");
-}
-
-  const resultat = await response.json();
-  console.log(resultat);
-
-   formulaire.reset();
-   window.location.href = "../pages/welcome.html";
-    } catch (error) {
-      console.error("Erreur :", error);
-
-      const errorMsg = document.createElement("p");
-      errorMsg.textContent = "Erreur lors de l’inscription !";
-      errorMsg.classList.add("text-red-600", "mt-2");
-      formulaire.appendChild(errorMsg);
+      throw new Error("Échec de l’enregistrement");
     }
-  });
+
+    await response.json();
+
+    formulaire.reset();
+
+    window.location.href = "../pages/welcome.html";
+
+  } catch (error) {
+    console.error(error);
+    alert("Erreur lors de l’inscription");
+  }
 });
